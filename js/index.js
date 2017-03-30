163d141672e5f47904cd88747ce1f653f1f9d04a
Kerri Shotts <kerrishotts@gmail.com>
2017-03-29 23:04:49 -0500

    classify & fps

--- js/index.js
+++ js/index.js
@@ -1,5 +1,70 @@
-var camera, cLight, scene, renderer;
-var spd = 15;
+/* globals THREE */
+
+function fmt2(n) {
+    return Math.round(n * 100) / 100;
+}
+
+class FPS {
+    constructor(el, { reportEvery = 250, history = 60 } = {}) {
+        if (!el) {
+            this._el = document.createElement("div");
+            this._el.classList.add("fps");
+            document.body.appendChild(this._el);
+        } else {
+            this._el = el;
+        }
+
+        this.reportEvery = reportEvery;
+        this.history = history;
+
+        this.reset();
+    }
+
+    reset() {
+        this._recentFrameTimes = [];
+        this._recentTime = 0;
+
+        this._totalFrames = 0;
+        this._totalTime = 0;
+
+        this._oldTime = undefined;
+        this._lastReportTime = 0;
+    }
+
+    frame(time) {
+        let delta = 0;
+        if (this._oldTime !== undefined) {
+            delta = time - this._oldTime;
+            this._totalTime += delta;
+            this._totalFrames++;
+
+            this._recentFrameTimes.push(delta);
+            this._recentTime += delta;
+            while (this._recentFrameTimes.length > this.history) {
+                this._recentTime -= this._recentFrameTimes.shift();
+            }
+        }
+        this._oldTime = time;
+        if ((time - this._lastReportTime) > this.reportEvery) {
+            this.report();
+            this._lastReportTime = time;
+        }
+        return delta;
+    }
+
+    report() {
+        let recentAvgFrameTime = this._recentTime / this._recentFrameTimes.length;
+        let recentFPS = 1000 / recentAvgFrameTime;
+        let recentFrameTimes = this._recentFrameTimes.reduce((acc, v, idx, arr) => {
+            if (idx > arr.length - 60) {
+                acc.push(["&#x2804;", "&#x2806;", "&#x2807;"][Math.min(Math.floor(v / 18), 2)]);
+            }
+            return acc;
+        }, []).join("");
+        this._el.innerHTML = `rFPS: ${fmt2(recentFPS)} (<span style='letter-spacing: -0.5em'>${recentFrameTimes}</span> )`;
+    }
+}
+
 
 class Level {
     _initBoxArray() {
@@ -7,42 +72,39 @@ class Level {
             drawDistance = this.drawDistance,
             level = this.level,
             _boxes = this._boxes;
-            
-        let srcMaterials = [0,1].map(v => new THREE.MeshLambertMaterial({
+
+        let srcMaterials = [0, 1].map(v => new THREE.MeshLambertMaterial({
             color: (v === 0) ? 0xFF8020 : 0x8020FF, wireframe: false
         }));
-        
-  	     let box = new THREE.BoxGeometry(blockSize, 100*blockSize, blockSize, 1, 1, 1);
-        
+
+        let box = new THREE.BoxGeometry(blockSize, 100 * blockSize, blockSize, 1, 1, 1);
+
         for (let z = 0; z < drawDistance; z++) {
             _boxes.push(level[0].map((_, idx) => new THREE.Mesh(box, srcMaterials[(z + idx) % 2])));
         }
     }
-  
-    constructor(level, {blockSize = 200, stepSize = 25, drawDistance = 20} = {}) {
+
+    constructor(level, { blockSize = 200, stepSize = 25, drawDistance = 20 } = {}) {
         this.blockSize = blockSize;
         this.stepSize = stepSize;
         this.drawDistance = drawDistance;
-        this.level = level.map(r => r.split(" ").map(c => Number.isNaN(Number(c)) ? c : Number(c))); 
+        this.level = level.map(r => r.split(" ").map(c => Number.isNaN(Number(c)) ? c : Number(c)));
         this.curRow = 0;
         this.maxVisibleRow = drawDistance - 1;
         this._boxes = [];
         this._initBoxArray();
     }
-  
+
     get description() {
-      return this.level.map(r => r.join(" ")).join(String.fromCharCode(10));  
+        return this.level.map(r => r.join(" ")).join(String.fromCharCode(10));
     }
-  
+
     makeScene() {
         let scene = new THREE.Scene();
-    
-        this.updateScene(0, {force: true});
+
+        this.updateScene(0, { force: true });
         this._boxes.forEach(z => z.forEach(mesh => scene.add(mesh)));
-    
-        //let aLight = new THREE.AmbientLight(0xFFFFFF, 0.25);
-        //scene.add(aLight); 
-    
+
         let hLight = new THREE.HemisphereLight(0xFFFFFF, 0x000000, 1);
         scene.add(hLight);
 
@@ -51,181 +113,198 @@ class Level {
         scene.add(dLight);
 
         return scene;
- 
-    }
-  
-  updateScene(cameraZ, {force = false} = {}) {
-		let stepSize = this.stepSize, blockSize = this.blockSize, level = this.level,
-        drawDistance = this.drawDistance, _boxes = this._boxes;
 
-    let curRow = Math.max(Math.floor(-(cameraZ) / blockSize), 0);
-    let maxVisibleRow = curRow + drawDistance - 1;
-    let delta = curRow - this.curRow;
-    
-    if (force || delta > 0) {
-      let offsetY = 50 * stepSize, halfHeight = 50 * blockSize;
-      
-      for (let i = 0; i < delta; i++) {
-      	let row = _boxes.shift();
-        _boxes.push(row);
-      }
-      
-      for (let z = force ? curRow : (maxVisibleRow - delta); z <= Math.min(level.length - 1, maxVisibleRow); z++) {
-        let r = level[z];
-        let offsetX = (r.length / 2) * blockSize - (blockSize / 2);
-        for (let x = r.length-1; x > -1; x--) {
-          let c = r[x];
-          let mesh = _boxes[z - curRow][x];
-          if (c !== "..") {
-            if (c === 0) {
-              //c = Math.floor(Math.abs((Math.sin(z/6.28) + Math.cos(x/6.28)) * 25));
+    }
+
+    updateScene(cameraZ, { force = false } = {}) {
+        let stepSize = this.stepSize, blockSize = this.blockSize, level = this.level,
+            drawDistance = this.drawDistance, _boxes = this._boxes;
+
+        let curRow = Math.max(Math.floor(-(cameraZ) / blockSize), 0);
+        let maxVisibleRow = curRow + drawDistance - 1;
+        let delta = curRow - this.curRow;
+
+        if (force || delta > 0) {
+            let offsetY = 50 * stepSize, halfHeight = 50 * blockSize;
+
+            for (let i = 0; i < delta; i++) {
+                let row = _boxes.shift();
+                _boxes.push(row);
             }
-            let h = c * stepSize;
-            mesh.visible = true;
-            mesh.position.set(x * blockSize - offsetX, -(halfHeight + offsetY) + h, -z * blockSize);
-          } else {
-            mesh.visible = false;
-          }
-        }        
-      }
-    }
-    
-    this.curRow = curRow;
-    this.maxVisibleRow = maxVisibleRow;
-  }
-  
-  heightAtCoordinates(z, x) {
-    let offsetY = 50 * this.stepSize, halfHeight = 50 * this.blockSize;
-  	let r = this.level[z];
-    if (r) {
-      let c = r[x];
-      if (Number.isNaN(Number(c))) {
-        return undefined;
-      }
-			if (c === 0) {
-                    //c = Math.floor(Math.abs((Math.sin(z/6.28) + Math.cos(x/6.28)) * 25));
-      }
-      let h =  -offsetY + (Number(c) * this.stepSize);
-      return h;
-    } else {
-    	return undefined;
-    }
-  }
-  
-  heightAtPosition(position) {
-  	let blockSize = this.blockSize;
-    let offsetX = (this.level[0].length / 2) * blockSize - (blockSize / 2);
 
-  	return this.heightAtCoordinates(Math.floor(-(position.z / blockSize) + 0.5), Math.floor((position.x + offsetX) / blockSize));
-  }
+            for (let z = force ? curRow : (maxVisibleRow - delta); z <= Math.min(level.length - 1, maxVisibleRow); z++) {
+                let r = level[z];
+                let offsetX = (r.length / 2) * blockSize - (blockSize / 2);
+                for (let x = r.length - 1; x > -1; x--) {
+                    let c = r[x];
+                    let mesh = _boxes[z - curRow][x];
+                    if (c !== "..") {
+                        let h = c * stepSize;
+                        mesh.visible = true;
+                        mesh.position.set(x * blockSize - offsetX, -(halfHeight + offsetY) + h, -z * blockSize);
+                    } else {
+                        mesh.visible = false;
+                    }
+                }
+            }
+        }
+
+        this.curRow = curRow;
+        this.maxVisibleRow = maxVisibleRow;
+    }
+
+    heightAtCoordinates(z, x) {
+        let offsetY = 50 * this.stepSize;
+        let r = this.level[z];
+        if (r) {
+            let c = r[x];
+            if (Number.isNaN(Number(c))) {
+                return undefined;
+            }
+            let h = -offsetY + (Number(c) * this.stepSize);
+            return h;
+        } else {
+            return undefined;
+        }
+    }
+
+    heightAtPosition(position) {
+        let blockSize = this.blockSize;
+        let offsetX = (this.level[0].length / 2) * blockSize - (blockSize / 2);
+
+        return this.heightAtCoordinates(Math.floor(-(position.z / blockSize) + 0.5), Math.floor((position.x + offsetX) / blockSize));
+    }
 }
 
-var level = new Level([
-  ".. 00 00 00 00 00 00 00 50 00 00 00 00 00 00 00 ..",
-  ".. 50 50 50 50 50 50 50 50 50 50 50 50 50 50 50 ..",
-  ".. 51 51 51 51 51 51 51 51 51 51 51 51 51 51 51 ..",
-  ".. 00 00 00 00 60 60 52 52 52 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 60 60 53 53 53 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 60 60 54 54 54 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 60 60 55 55 55 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 56 56 56 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 57 57 57 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 58 00 58 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 59 00 59 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 60 00 60 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 01 00 00 70 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 02 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 03 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 04 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 05 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 06 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 07 00 00 00 30 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 08 00 00 00 40 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 09 00 00 00 50 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 10 00 00 00 70 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 15 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 20 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 25 00 00 00 99 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 30 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 35 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 35 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 40 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 40 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 40 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 45 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 45 00 00 00 00 00 00 00 ..",
-  ".. 00 00 00 00 00 00 00 45 00 00 00 00 00 00 00 ..",
-])
+class Game {
+    constructor() {
+        this.camera = undefined;
+        this.scene = undefined;
+        this.renderer = undefined;
+        this.paused = false;
 
+        this.fps = new FPS();
 
-document.getElementById("l").textContent = level.description;
+        this.spd = 15;
 
-init();
-animate();
+        this.level = new Level([
+            ".. 00 00 00 00 00 00 00 50 00 00 00 00 00 00 00 ..",
+            ".. 50 50 50 50 50 50 50 50 50 50 50 50 50 50 50 ..",
+            ".. 51 51 51 51 51 51 51 51 51 51 51 51 51 51 51 ..",
+            ".. 00 00 00 00 60 60 52 52 52 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 60 60 53 53 53 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 60 60 54 54 54 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 60 60 55 55 55 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 56 56 56 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 57 57 57 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 58 00 58 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 59 00 59 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 60 00 60 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 01 00 00 70 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 02 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 03 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 04 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 05 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 06 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 07 00 00 00 30 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 08 00 00 00 40 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 09 00 00 00 50 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 10 00 00 00 70 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 15 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 20 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 25 00 00 00 99 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 30 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 35 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 35 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 40 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 40 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 40 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 45 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 45 00 00 00 00 00 00 00 ..",
+            ".. 00 00 00 00 00 00 00 45 00 00 00 00 00 00 00 ..",
+        ])
 
-function init() {
-    camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.set(0,200, 2000);
+        document.getElementById("level").textContent = this.level.description;
 
-		scene = level.makeScene();
-				scene.fog = new THREE.FogExp2( 0x000000, 0.00033 );
+    }
 
-    cLight = new THREE.PointLight(0xFFFFFF, 1, 5000, 2);
-    cLight.position.set(camera.position.x, camera.position.y, camera.position.z);
-    //scene.add(cLight);
+    start() {
+        this.init();
+        this.resume();
+    }
 
-    renderer = new THREE.WebGLRenderer({
-        antialias: false
-    });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(devicePixelRatio);
+    pause() {
+        this.paused = true;
+    }
 
-    document.body.appendChild(renderer.domElement);
+    resume() {
+        this.paused = false;
+        this.fps.reset();
+        requestAnimationFrame(t => this.animate(t));
+    }
 
+    init() {
+        this.paused = false;
 
+        this.camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 10000);
+        this.camera.position.set(0, 200, 2000);
 
+        this.scene = this.level.makeScene();
+        this.scene.fog = new THREE.FogExp2(0x000000, 0.00033);
+
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: false
+        });
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setPixelRatio(devicePixelRatio);
+
+        document.body.appendChild(this.renderer.domElement);
+
+        document.getElementById("toggle").addEventListener("click", () => this.toggleState());
+    }
+
+    animate(t) {
+        if (!this.paused) {
+            requestAnimationFrame(t => this.animate(t));
+        }
+
+        let camera = this.camera,
+            scene = this.scene,
+            level = this.level,
+            renderer = this.renderer,
+            spd = this.spd;
+
+        let d = this.fps.frame(t);
+
+        //    camera.position.x += 5;
+        let camHeight = level.heightAtPosition(camera.position);
+        if (camHeight) {
+            camHeight += 200;
+            if (camHeight !== camera.position.y) {
+                camera.position.y += (camHeight - camera.position.y) / (100 / spd);
+            }
+        }
+        camera.position.z -= spd * (d / 16.67);
+        if (camera.position.z < -(level.level.length * level.blockSize)) {
+            camera.position.z = 2000;
+            level.updateScene(camera.position.z, { force: true });
+        } else {
+            level.updateScene(camera.position.z);
+        }
+
+        renderer.render(scene, camera);
+    }
+
+    toggleState() {
+        if (this.paused) {
+            this.resume();
+        } else {
+            this.pause();
+        }
+    }
 }
 
-var ot = 0, f = 0, tf = 0, td = 0, rf=0;
-
-function animate(t) {
-    var d = 0;
-    f++; tf++;
-    requestAnimationFrame(animate);
-
-    if (ot != 0 && t) {
-       d = t - ot;
-       td += d;
-       if (td > 1000) {
-       		 rf = Math.floor(10000 / (td/f))/10;
-           td-=1000; f = 0;
-		document.getElementById("d").textContent = 
-      [Math.floor(t), Math.floor(d), Math.floor(camera.position.z), f, tf, rf, Math.floor(10000/ (td/f))/10].join(", ");
-
-       }
-    }
-    ot = t;
-
-//    camera.position.x += 5;
-		let camHeight = level.heightAtPosition(camera.position);
-    if (camHeight) {
-			camHeight += 200;
-      if (camHeight !== camera.position.y) {
-      	camera.position.y += (camHeight - camera.position.y)/(100/spd);
-      }
-    }
-    camera.position.z -= spd * (d/16.67);
-    if (camera.position.z < -(level.level.length * level.blockSize)) {
-        camera.position.z = 2000;
-        level.updateScene(camera.position.z, {force: true});
-    } else {
-    	level.updateScene(camera.position.z);
-    }
-   //cLight.position.set(camera.position.x, camera.position.y + 500, camera.position.z + 500);
-
-
-    renderer.render(scene, camera);
-
-}
+let game = new Game();
+game.start();
