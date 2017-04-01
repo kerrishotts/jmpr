cd31d5ef9c9d84405600108c01b1664f0bb4993b
Kerri Shotts <kerrishotts@gmail.com>
2017-04-01 17:46:52 -0500

    bpm functionality

--- js/Level.js
+++ js/Level.js
@@ -1,31 +1,57 @@
 /* globals exports, require, THREE */
+const MS_IN_A_MINUTE = 60 * 1000;
+
 exports.Level = class Level {
+    constructor(level, { blockSize = 200, stepSize = 25, drawDistance = 20,
+        bpm = 120, colors = [0xFF8020, 0x8020FF], beatIntensity = 0.33 } = {}) {
+        this.blockSize = blockSize;
+        this.stepSize = stepSize;
+        this.drawDistance = drawDistance;
+
+        this.level = this._parseLevel(level);
+        this.curRow = 0;
+        this.maxVisibleRow = drawDistance - 1;
+
+        this.bpm = bpm;
+        this._timeLastBeat = 0;
+        this._bpmPosition = new THREE.Vector3();
+
+        this.colors = colors;
+        this.beatIntensity = beatIntensity;
+//        this.colors = colors.map(c => new THREE.Color(c));
+
+        this._boxes = [];
+        this._initBoxArray();
+    }
+
+    set bpm (v) {
+        this._bpm = v;
+        this._msBetweenBeats = v ? (MS_IN_A_MINUTE) / v : 0;
+    }
+
+    get bpm() {
+        return this._bpm;
+    }
+
+    startBeat() {
+        this._timeLastBeat = performance.now();
+    }
+
     _initBoxArray() {
         let blockSize = this.blockSize,
             drawDistance = this.drawDistance,
             level = this.level,
             _boxes = this._boxes;
 
-        let srcMaterials = [0, 1].map(v => new THREE.MeshLambertMaterial({
-            color: (v === 0) ? 0xFF8020 : 0x8020FF, wireframe: false
+        let srcMaterials = this.colors.map(v => new THREE.MeshLambertMaterial({
+            color: v, wireframe: false
         }));
 
         let box = new THREE.BoxGeometry(blockSize, 100 * blockSize, blockSize, 1, 1, 1);
 
         for (let z = 0; z < drawDistance; z++) {
-            _boxes.push(level[0].map((_, idx) => new THREE.Mesh(box, srcMaterials[(z + idx) % 2])));
+            _boxes.push(level[0].map((_, idx) => new THREE.Mesh(box, srcMaterials[(z + idx) % (this.colors.length)])));
         }
-    }
-
-    constructor(level, { blockSize = 200, stepSize = 25, drawDistance = 20 } = {}) {
-        this.blockSize = blockSize;
-        this.stepSize = stepSize;
-        this.drawDistance = drawDistance;
-        this.level = this._parseLevel(level);
-        this.curRow = 0;
-        this.maxVisibleRow = drawDistance - 1;
-        this._boxes = [];
-        this._initBoxArray();
     }
 
     _parseLevel(level) {
@@ -75,6 +101,15 @@ exports.Level = class Level {
         let maxVisibleRow = curRow + drawDistance - 1;
         let delta = curRow - this.curRow;
 
+        let now = performance.now();
+        let timeForBeat = this._timeLastBeat !== 0 ? (now - this._timeLastBeat) > this._msBetweenBeats : false;
+        let beatIntensity = this.beatIntensity,
+            msBetweenBeats = this._msBetweenBeats,
+            timeSinceLastBeat = msBetweenBeats ? (now - this._timeLastBeat) % msBetweenBeats : 0,
+            colors = this.colors, numColors = colors.length;
+
+
+        // move boxes as needed to the end of the level
         if (force || delta > 0) {
             let offsetY = 50 * stepSize, halfHeight = 50 * blockSize;
 
@@ -100,8 +135,27 @@ exports.Level = class Level {
             }
         }
 
+        // colors get change irrespective of adjusting visible boxes
+        for (let z = curRow; z <= Math.min(level.length - 1, maxVisibleRow); z++) {
+            let r = level[z], dz = z - curRow, maxDistance = Math.sqrt(Math.pow(drawDistance, 2) + Math.pow(r.length - 1, 2));
+            for (let x = r.length - 1; x > -1; x--) {
+                let mesh = _boxes[dz][x];
+                mesh.material.color.set(colors[(z + x) % numColors]);
+                if (msBetweenBeats > 0) {
+                    var distanceFromZero = Math.sqrt(Math.pow(drawDistance - dz, 2) + (x * x));
+                    distanceFromZero = 1/(distanceFromZero / maxDistance);
+                    var lOffset = ((1 - (timeSinceLastBeat / msBetweenBeats)) * beatIntensity); // * distanceFromZero;
+                    mesh.material.color.offsetHSL(0, 0, lOffset);
+                }
+            }
+        }
+
         this.curRow = curRow;
         this.maxVisibleRow = maxVisibleRow;
+
+        if (timeForBeat) {
+            this._timeLastBeat = now;
+        }
     }
 
     heightAtCoordinates(z, x) {
@@ -121,7 +175,7 @@ exports.Level = class Level {
 
     heightAtPosition(position) {
         let blockSize = this.blockSize;
-        let offsetX = (this.level[0].length / 2) * blockSize - (blockSize / 2);
+        let offsetX = (this.level[0].length / 2) * blockSize; // - (blockSize / 2);
 
         return this.heightAtCoordinates(Math.floor(-(position.z / blockSize) + 0.5), Math.floor((position.x + offsetX) / blockSize));
     }
@@ -134,33 +188,34 @@ exports.Level = class Level {
         return [
             [".. 50 50 50 50 50 50 50 50 50 50 50 50 50 50 50 ..", 25],
             [".. 51 51 51 51 51 51 51 51 51 51 51 51 51 51 51 ..", 2],
-            ".. 00 00 00 00 60 60 52 52 52 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 60 60 53 53 53 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 60 60 54 54 54 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 60 60 55 55 55 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 56 56 56 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 57 57 57 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 58 00 58 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 59 00 59 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 60 00 60 00 00 00 00 00 00 ..",
-            [".. 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ..", 2],
-            ".. 00 00 00 00 00 00 00 01 00 00 70 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 02 00 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 03 00 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 04 00 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 05 00 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 06 00 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 07 00 00 00 30 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 08 00 00 00 40 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 09 00 00 00 50 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 10 00 00 00 70 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 15 00 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 20 00 00 00 00 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 25 00 00 00 99 00 00 00 ..",
-            ".. 00 00 00 00 00 00 00 30 00 00 00 00 00 00 00 ..",
-            [".. 00 00 00 00 00 00 00 35 00 00 00 00 00 00 00 ..", 2],
-            [".. 00 00 00 00 00 00 00 40 00 00 00 00 00 00 00 ..", 3],
-            [".. 00 00 00 00 00 00 00 45 00 00 00 00 00 00 00 ..", 3],
+             ".. .. .. .. .. 99 99 52 52 52 99 99 .. .. .. .. ..",
+             ".. .. .. .. .. 99 99 53 53 53 99 99 .. .. .. .. ..",
+             ".. .. .. .. .. 99 99 54 54 54 99 99 .. .. .. .. ..",
+             ".. .. .. .. .. 99 99 55 55 55 99 99 .. .. .. .. ..",
+             ".. .. .. .. .. .. .. 56 56 56 .. .. .. .. .. .. ..",
+             ".. .. .. .. .. .. .. 57 57 57 .. .. .. .. .. .. ..",
+             ".. .. .. .. .. .. .. 58 .. 58 .. .. .. .. .. .. ..",
+             ".. .. .. .. .. .. .. 59 57 59 .. .. .. .. .. .. ..",
+             ".. .. .. .. .. .. .. 60 57 60 .. .. .. .. .. .. ..",
+            [".. .. .. .. .. .. .. .. 57 .. .. .. .. .. .. .. ..", 2],
+             ".. .. .. .. .. .. .. .. .. .. .. 70 .. .. .. .. ..",
+            [".. .. .. .. .. .. .. .. 57 .. .. .. .. .. .. .. ..", 25],
+             ".. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 57 .. .. .. .. .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 47 .. .. .. .. .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 45 .. .. .. .. .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 41 .. .. .. .. .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 40 .. .. .. 30 .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 40 .. .. .. 40 .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 40 .. .. .. 50 .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 45 .. .. .. 70 .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 45 .. .. .. .. .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 45 .. .. .. .. .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 45 .. .. .. 99 .. .. .. ..",
+             ".. .. .. .. .. .. .. .. 45 .. .. .. .. .. .. .. ..",
+            [".. .. .. .. .. .. .. .. 45 .. .. .. .. .. .. .. ..", 2],
+            [".. .. .. .. .. .. .. .. 45 .. .. .. .. .. .. .. ..", 3],
+            [".. .. .. .. .. .. .. .. 45 .. .. .. .. .. .. .. ..", 3],
         ];
     }
 
