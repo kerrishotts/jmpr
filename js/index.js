b8564543068517b422dbfe3ac40a0fa12e40f139
Kerri Shotts <kerrishotts@gmail.com>
2017-04-02 00:01:24 -0500

    controllers

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 /* globals require*/
 const Game = require("./Game");
-let game = new Game();
+const TouchController = require("./TouchController");
+const KeyboardController = require("./KeyboardController");
+const ControllerCollection = require("./ControllerCollection");
+
+let kbd = new KeyboardController();
+let touch = new TouchController();
+let controllers = new ControllerCollection([kbd, touch]);
+let game = new Game(controllers);
 game.start();
