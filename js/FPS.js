50478f1ac2ec87d6e5f21aca21600769b771dbda
Kerri Shotts <kerrishotts@gmail.com>
2017-04-02 00:00:50 -0500

    move el into opts

--- js/FPS.js
+++ js/FPS.js
@@ -1,7 +1,7 @@
 /*globals exports, require*/
 const fmt2 = require("./util").fmt2;
 exports.FPS = class FPS {
-    constructor(el, { reportEvery = 250, history = 60 } = {}) {
+    constructor({ el, reportEvery = 250, history = 60, fancy = false } = {}) {
         if (!el) {
             this._el = document.createElement("div");
             this._el.classList.add("fps");
@@ -12,6 +12,7 @@ exports.FPS = class FPS {
 
         this.reportEvery = reportEvery;
         this.history = history;
+        this.fancy = fancy;
 
         this.reset();
     }
@@ -54,14 +55,17 @@ exports.FPS = class FPS {
     report() {
         let recentAvgFrameTime = this._recentTime / this.history;
         let recentFPS = 1000 / recentAvgFrameTime;
-        let lastFrameTime = (this._recentFrameTimes[this._recentFrameTimes.length - 1]);
-        /*let recentFrameTimes = this._recentFrameTimes.reduce((acc, v, idx, arr) => {
-            if (idx > arr.length - 60) {
-                acc.push(["&#x2804;", "&#x2806;", "&#x2807;"][Math.min(Math.floor(v / 18), 2)]);
-            }
-            return acc;
-        }, []).join("");*/
-        //this._el.innerHTML = `${Math.floor(this._oldTime)} rFPS: ${fmt2(recentFPS)} (<span style='letter-spacing: -0.5em'>${recentFrameTimes}</span> ${this._extraInfo} )`;
-        this._el.innerHTML = `${lastFrameTime} rFPS: ${fmt2(recentFPS)} ${this._extraInfo}`;
+        let lastFrameTime = Math.floor((this._recentFrameTimes[this._recentFrameTimes.length - 1]) * 100) / 100;
+        if (this.fancy) {
+            let recentFrameTimes = this._recentFrameTimes.reduce((acc, v, idx, arr) => {
+                if (idx > arr.length - 60) {
+                    acc.push(["&#x2804;", "&#x2806;", "&#x2807;"][Math.min(Math.floor(v / 18), 2)]);
+                }
+                return acc;
+            }, []).join("");
+            this._el.innerHTML = `${Math.floor(lastFrameTime)} rFPS: ${fmt2(recentFPS)} (<span style='letter-spacing: -0.5em'>${recentFrameTimes}</span> ) ${this._extraInfo}`;
+        } else {
+            this._el.innerHTML = `${lastFrameTime} rFPS: ${fmt2(recentFPS)} ${this._extraInfo}`;
+        }
     }
 }
