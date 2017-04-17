dc05c8fd637c06eed572af12c128a8da44f30fe5
Kerri Shotts <kerrishotts@gmail.com>
2017-04-16 23:03:20 -0500

    clamp and sign

--- js/util.js
+++ js/util.js
@@ -2,5 +2,17 @@
 exports.util = {
     fmt2(n) {
         return Math.round(n * 100) / 100;
+    },
+    clamp(v, min, max) {
+        if (v < min) {
+            return min;
+        }
+        if (v > max) {
+            return max;
+        }
+        return v;
+    },
+    sign(v) {
+        return v < 0 ? -1 : 1;
     }
 };
