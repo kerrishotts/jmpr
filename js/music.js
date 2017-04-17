2723a7a7b2493d136e07ff1d8f9cb5ab22218e73
Kerri Shotts <kerrishotts@gmail.com>
2017-04-16 23:01:53 -0500

    split out music info

---
+++ js/music.js
@@ -0,0 +1,9 @@
+/* globals exports */
+
+exports.music = {
+    "roccow01": {
+        bpm: 128.005,
+        file: "roccow-sweet-self-satisfaction.mp3",
+        startPoints: [0, 59.95, 120.1]
+    }
+}
