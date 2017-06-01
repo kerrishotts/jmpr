b91d76b7c5752a7878a8f883291bd49993aa9b56
Kerri Shotts <kerrishotts@gmail.com>
2017-05-31 21:06:33 -0500

    allow logging long deltas if we need

--- www/js/Delta.js
+++ www/js/Delta.js
@@ -2,6 +2,17 @@ export default class Delta {
     constructor({ t = -1, maxAcceptableDelta = 67 } = {}) {
         this._t = t;
         this.maxAcceptableDelta = maxAcceptableDelta;
+        this.log = false;
+        this._logGroup = undefined;
+    }
+    set logGroup(g) {
+        this._logGroup = g;
+        if (typeof this.log === "number") {
+            console.groupEnd();
+            if (g) {
+                console.group(g);
+            }
+        }
     }
     reset() {
         this._t = -1;
@@ -15,9 +26,15 @@ export default class Delta {
         if (delta < 0) {
             delta = 0;
         }
+
+        if (typeof this.log === "number" && delta > this.log && this._logGroup) {
+            console.log(`${performance.now()}: ${delta}`);
+        }
+
         if (delta > this.maxAcceptableDelta) {
             delta = this.maxAcceptableDelta;
         }
+
         return delta;
     }
 }
