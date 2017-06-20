0ac1da21c0f888ab2dcf89f4bf00b0b5795a9742
Kerri Shotts <kerrishotts@gmail.com>
2017-06-20 01:40:51 -0500

    inverse

--- www/js/textures.js
+++ www/js/textures.js
@@ -11,7 +11,8 @@ let textures = Object.keys(flags.flags).reduce((acc, flag) => {
         let line = t[r].split("").reverse();
         for (let c = 0; c < line.length; c++) {
             let arrIdx = (((H * W) - 1) - (r * W + c)) * 4,
-                tValue = line[c] === "X" ? 1 : 0;
+                //tValue = line[c] === "X" ? 1 : 0;
+                tValue = line[c] === "X" ? 0 : 1;
             arr[arrIdx + 0] = (tValue * 255);
             arr[arrIdx + 1] = (tValue * 255);
             arr[arrIdx + 2] = (tValue * 255);
