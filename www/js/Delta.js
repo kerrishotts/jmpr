5bb90adc35f3ff8c274f124239da66fb215a2650
Kerri Shotts <kerrishotts@gmail.com>
2017-06-20 01:40:32 -0500

    disable logging for now

--- www/js/Delta.js
+++ www/js/Delta.js
@@ -7,17 +7,23 @@ export default class Delta {
     }
     set logGroup(g) {
         this._logGroup = g;
+
+        /*
         if (typeof this.log === "number") {
             console.groupEnd();
             if (g) {
                 console.group(g);
             }
         }
+        */
     }
     reset() {
         this._t = -1;
     }
-    update(t = 0) {
+    update(t) {
+        if (!t) {
+            t = 0;
+        }
         var delta = t - this._t;
         if (this._t < 0) {
             delta = 0;
@@ -27,9 +33,11 @@ export default class Delta {
             delta = 0;
         }
 
+        /*
         if (typeof this.log === "number" && delta > this.log && this._logGroup) {
             console.log(`${performance.now()}: ${delta}`);
         }
+        */
 
         if (delta > this.maxAcceptableDelta) {
             delta = this.maxAcceptableDelta;
